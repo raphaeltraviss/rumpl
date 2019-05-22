3e8750dd300f34353ed6270f164bb9aee3dd115c
Raphael Spencer <raphael@headway.io>
2019-05-22 16:36:17 -0500

    Chapter 3, Controllers complete

--- lib/rumpl_web/views/user_view.ex
+++ lib/rumpl_web/views/user_view.ex
@@ -1,3 +1,11 @@
 defmodule RumplWeb.UserView do
   use RumplWeb, :view
+
+  alias Rumpl.Accounts
+
+  def first_name(%Accounts.User{name: name}) do
+    name
+    |> String.split(" ")
+    |> Enum.at(0)
+  end
 end
