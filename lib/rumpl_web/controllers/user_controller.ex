5b6eb3168e7ebb686d0cae484b3c288178e0e8e3
Raphael Spencer <raphael@headway.io>
2019-05-24 11:13:36 -0500

    The UI can now create users

--- lib/rumpl_web/controllers/user_controller.ex
+++ lib/rumpl_web/controllers/user_controller.ex
@@ -2,8 +2,9 @@ defmodule RumplWeb.UserController do
   use RumplWeb, :controller
 
   alias Rumpl.Accounts
+  alias Rumpl.Accounts.User
 
-  def index(conn, params) do
+  def index(conn, _params) do
     users = Accounts.list_users()
     render(conn, "index.html", users: users)
   end
@@ -11,5 +12,18 @@ defmodule RumplWeb.UserController do
   def show(conn, %{"id" => id}) do
     user = Accounts.get_user(id)
     render(conn, "show.html", user: user)
+  end
+
+  def new(conn, _params) do
+    changeset = Accounts.change_user(%User{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{ "user" => user_params}) do
+    {:ok, user} = Accounts.create_user(user_params)
+
+    conn
+    |> put_flash(:info, "#{user.name} created!")
+    |> redirect(to: Routes.user_path(conn, :index))
   end
 end
--- lib/rumpl_web/router.ex
+++ lib/rumpl_web/router.ex
@@ -16,9 +16,8 @@ defmodule RumplWeb.Router do
   scope "/", RumplWeb do
     pipe_through :browser
 
-    get "/users", UserController, :index
-    get "/user/:id", UserController, :show
     get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
   end
 
   # Other scopes may use custom stacks.
