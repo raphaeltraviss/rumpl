cefa33e0c61650cf083295c8d0bea639df36fa59
Raphael Spencer <raphael@headway.io>
2019-05-28 10:59:14 -0500

    Auth plug scaffolded

--- lib/rumpl/accounts/user.ex
+++ lib/rumpl/accounts/user.ex
@@ -1,10 +1,12 @@
 defmodule Rumpl.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Rumpl.Accounts.Credential
 
   schema "users" do
     field :name, :string
     field :username, :string
+    has_one :credential, Credential
 
     timestamps()
   end
@@ -14,5 +16,11 @@ defmodule Rumpl.Accounts.User do
     |> cast(attrs, [:name, :username])
     |> validate_required([:name, :username])
     |> validate_length(:username, min: 1, max: 20)
+  end
+
+  def registration_changeset(user, attrs) do
+    user
+    |> changeset(attrs)
+    |> cast_assoc(:credential, with: &Credential.changeset/2, required: true)
   end
 end
--- lib/rumpl_web/router.ex
+++ lib/rumpl_web/router.ex
@@ -7,6 +7,7 @@ defmodule RumplWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug RumplWeb.Auth
   end
 
   pipeline :api do
